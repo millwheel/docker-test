1f0958b6406ffa61011d5972fe5521396e9fc83d
Sijun <sionwer4@naver.com>
2023-03-19 14:54:48 +0900

    Update container test for ECS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Docker test!");
+    res.send("Docker test! This has been updated");
 })
 
 app.listen(8080, () => console.log('Server is running'));
